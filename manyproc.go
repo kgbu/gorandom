c10c0f30772659c00dd67809260cd19eea08bb73
OCAO Kazutaka Ogaki <ocaokgbu@gmail.com>
2015-07-08 19:54:26 +0900

    wait all goroutines

--- manyproc.go
+++ manyproc.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"log"
+	"sync"
 	"time"
 )
 
 func main() {
 	var chans [200000]chan int
+	var wg sync.WaitGroup
+
 	for i := range chans {
    		chans[i] = make(chan int)
 	}
 	for i, ch := range chans {
+		wg.Add(1)
 		go func(i int, c chan int) {
+			defer wg.Done()
+
 			log.Printf("now receiving start %v, from chan %v", i, c)
 			select {
 			case cmd := <-c:
@@ -27,4 +33,6 @@ func main() {
 		log.Printf("sending %v, %v",i, ch)
 		ch <- i
 	}
+	wg.Wait()
+	log.Printf("now all processes completed")
 }
--- manyprocnolog.go
+++ manyprocnolog.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"log"
+	"sync"
 	"time"
 )
 
 func main() {
 	var chans [200000]chan int
+	var wg sync.WaitGroup
+
 	for i := range chans {
    		chans[i] = make(chan int)
 	}
 	for i, ch := range chans {
+		wg.Add(1)
 		go func(i int, c chan int) {
+			defer wg.Done()
+
 			select {
 			case cmd := <-c:
 			case <-time.After(100 * time.Second):
@@ -23,4 +29,5 @@ func main() {
 	for i, ch := range chans {
 		ch <- i
 	}
+	wg.Wait()
 }
