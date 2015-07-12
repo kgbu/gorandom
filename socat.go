0bc1f98d27b82a629a0ea3517a876a6253958879
OCAO Kazutaka Ogaki <ocaokgbu@gmail.com>
2015-07-12 14:38:16 +0900

    Scanner for pipe

--- socat.go
+++ socat.go
@@ -2,38 +2,26 @@ package main
 
 import (
 	"fmt"
+	"bufio"
 	"os"
 	"os/exec"
 )
 
 func main() {
-        cmd := exec.Command("socat", "-d", "-d", "pty,raw,echo=0", "pty,raw,echo=0")
-        serr, err := cmd.StderrPipe()
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
-        sout, err := cmd.StdoutPipe()
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
+	cmd := exec.Command("socat", "-d", "-d", "pty,raw,echo=0", "pty,raw,echo=0")
+	serr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	cmd.Start()
 
-	var buf []byte
-	_, err = sout.Read(buf)
-	if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-	}
-        fmt.Println(string(buf))
+	scanner := bufio.NewScanner(serr)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		fmt.Println()
+	}	
 
-	_, err = serr.Read(buf)
-	if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-	}
-        fmt.Println(string(buf))
 	cmd.Wait()
 }
